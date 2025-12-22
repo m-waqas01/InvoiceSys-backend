09b991c3d5512db1514dbb0409589c1f84992788
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-12-22 11:17:54 +0000

    update origin in cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,15 @@ const errorHandler = require("./middleware/errorMiddleware");
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://invoice-sys.vercel.app", // frontend URL
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
