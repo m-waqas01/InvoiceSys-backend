c57cbf09200bf16b53dd454806279dd11a28e711
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-12-23 15:08:35 +0000

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI;
+    const uri = process.env.MONGO_URI;
     if (!uri) throw new Error("MONGO_URI not set in env");
     await mongoose.connect(uri, {
       // useNewUrlParser: true, useUnifiedTopology: true // mongoose v7 handles it
