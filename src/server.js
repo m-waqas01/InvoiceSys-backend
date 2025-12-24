304c7a9307f1c979cbfb11b25ea706271fd3ff2d
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-12-24 18:35:49 +0000

    first commit

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
